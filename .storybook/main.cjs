3028c0c339961a48fe80e9f6263ef9de4ac6c82a
presentyourlove <jasonwth0928@gmail.com>
2026-01-23 16:38:17 +0800

    style: apply prettier formatting

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -6,16 +6,16 @@ const { join, dirname } = require('path');
  * It is needed in projects that use Yarn PnP or are set up within a monorepo.
  */
 function getAbsolutePath(value) {
-    return dirname(require.resolve(join(value, 'package.json')));
+  return dirname(require.resolve(join(value, 'package.json')));
 }
 
 /** @type { import('@storybook/html').StorybookConfig } */
 const config = {
-    stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-    addons: ['@storybook/addon-essentials', '@storybook/blocks'],
-    framework: {
-        name: getAbsolutePath('@storybook/html-vite'),
-        options: {},
-    },
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: ['@storybook/addon-essentials', '@storybook/blocks'],
+  framework: {
+    name: getAbsolutePath('@storybook/html-vite'),
+    options: {},
+  },
 };
 module.exports = config;
--- src/components/Footer.stories.js
+++ src/components/Footer.stories.js
@@ -1,12 +1,12 @@
 import Footer from './Footer.astro';
 
 export default {
-    title: 'Components/Footer',
-    component: Footer,
-    tags: ['autodocs'],
-    parameters: {
-        layout: 'fullscreen',
-    },
+  title: 'Components/Footer',
+  component: Footer,
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'fullscreen',
+  },
 };
 
 export const Default = {};
